6ab953d4da8376ebb6e9b975e3f71d91dfc075ff
Artem Pankov <artem.alwayshere@gmail.com>
2023-04-26 12:49:39 +0500

    Добавлен парсинг даты публикации

--- HHParser.Business/Models/JobMessage.cs
+++ HHParser.Business/Models/JobMessage.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace HHParser.Business.Models
 {
@@ -10,6 +11,13 @@ namespace HHParser.Business.Models
     {
         public string Title { get; set; }
         public string JobUrl { get; set; }
-        //public string UniqId { get; set; }
+        public DateTime PublishDate { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is JobMessage message) return JobUrl == message.JobUrl;
+            return false;
+        }
+        public override int GetHashCode() => JobUrl.GetHashCode();
     }
 }
--- HHParser.Worker/Worker.cs
+++ HHParser.Worker/Worker.cs
@@ -1,7 +1,9 @@
 using HHParser.Business.Models;
 using HtmlAgilityPack;
+using System.Net;
 using System.Text.RegularExpressions;
 using System.Xml;
+using System.Xml.Linq;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 
@@ -22,50 +24,54 @@ namespace HHParser.Worker
         {
             var botClient = new TelegramBotClient(apiKey);
             var me = await botClient.GetMeAsync();
-
-            List<JobMessage> jobs = new List<JobMessage>();
+            var tempMessages = new List<JobMessage>();
 
             while (!stoppingToken.IsCancellationRequested)
             {
 
-                //var url = "https://ekaterinburg.hh.ru/search/vacancy?no_magic=true&L_save_area=true&text=.net&excluded_text=&salary=&currency_code=RUR&experience=between1And3&schedule=remote&order_by=relevance&search_period=1&items_on_page=20";
-                var url = "https://ekaterinburg.hh.ru/search/vacancy?enable_snippets=true&experience=between1And3&no_magic=true&ored_clusters=true&schedule=remote&text=.net&search_period=0";
+                var url = "https://ekaterinburg.hh.ru/search/vacancy?no_magic=true&L_save_area=true&text=.Net&excluded_text=%D0%BC%D0%B5%D0%BD%D0%B5%D0%B4%D0%B6%D0%B5%D1%80%2C+%D0%B0%D0%B4%D0%BC%D0%B8%D0%BD%D0%B8%D1%81%D1%82%D1%80%D0%B0%D1%82%D0%BE%D1%80%2C+%D1%8E%D1%80%D0%B8%D1%81%D1%82&salary=&currency_code=RUR&experience=doesNotMatter&schedule=remote&order_by=publication_time&search_period=1&items_on_page=20";
                 var web = new HtmlWeb();
                 var doc = web.Load(url);
-                List<HtmlNode> myParagraphs = doc.DocumentNode.SelectNodes("//a[@class='serp-item__title']").ToList();
+                var aTags = doc.DocumentNode.SelectNodes("//a[@class='serp-item__title']");
 
-                List<JobMessage> newJobs = new List<JobMessage>();
 
-                foreach (var node in myParagraphs)
+                List<string> vacacyHrefs = new List<string>();
+
+                var messages = new List<JobMessage>();
+
+                foreach (var page in aTags)
                 {
-                    //string url1 = node.Attributes["href"].Value;
-                    //string id = url1.Split('/')[4].Split('?')[0];вфвфывф
+                    var href = page.Attributes["href"].Value;
+                    doc = web.Load(href);
+                    JobMessage message = new JobMessage();
 
-                    newJobs.Add(new JobMessage()
+                    foreach (var item in doc.DocumentNode.SelectNodes("//p[@class='vacancy-creation-time-redesigned']/span"))
                     {
-                        Title = node.InnerText,
-                        JobUrl = node.Attributes["href"].Value
-                        //UniqId = id
-                    });
+                        message.PublishDate = DateTime.Parse(item.InnerText);
+                    }
+                    message.Title = page.InnerText;
+                    message.JobUrl = href;
+                    messages.Add(message);
                 }
 
-
-                if (jobs.Count == 0)
+                if (tempMessages.Count == 0)
                 {
-                    await botClient.SendTextMessageAsync(chatId, $"{newJobs[0].Title} \n {newJobs[0].JobUrl} \n Вакансия при перезапуске.");
-                    jobs = newJobs;
+                    await botClient.SendTextMessageAsync(chatId, $"Самая новая на данный моент вакансия \n {messages[0].Title} \n Дата публикации - {messages[0].PublishDate} \n {messages[0].JobUrl} \n");
+                    tempMessages = messages;
                     _logger.LogInformation("Первый запрос: {time}", DateTimeOffset.Now);
                 }
 
-                else if (jobs.Count > 0 && jobs.SequenceEqual(newJobs))
-                { 
-                    var diffArr = newJobs.Except(jobs);
-
-                    foreach (var item in diffArr)
+                else if (!messages.SequenceEqual(tempMessages))
+                {
+                    foreach (var item in messages)
                     {
-                        await botClient.SendTextMessageAsync(chatId, $"Новая вакансия!!!! \n {item.Title} \n {item.JobUrl}");
+                        if (!tempMessages.Contains(item))
+                        {
+                            await Console.Out.WriteLineAsync(item.Title);
+                            await botClient.SendTextMessageAsync(chatId, $"Новая вакансия!!!! \n {item.Title} \n Дата публикации - {item.PublishDate}\n {item.JobUrl}");
+                        }
                     }
-                    jobs = newJobs;
+                    tempMessages = messages;
 
                     _logger.LogInformation("Появилась новая вакансия: {time}", DateTimeOffset.Now);
                 }
